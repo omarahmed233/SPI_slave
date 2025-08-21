54567a0952f0745b9aa45a983bd68e4b74c78b9a
Omar Ahmed <142747254+omarahmed233@users.noreply.github.com>
2025-08-21 12:40:33 +0300

    Update SPI_interface.v

--- SPI_interface.v
+++ SPI_interface.v
@@ -3,7 +3,7 @@ module SPI_interface(
     input MOSI, SS_n,
     input tx_valid,
     input [7:0] tx_data,
-    output MISO,
+    output reg MISO,
     output rx_valid,
     output reg [9:0] rx_data
 );
@@ -13,46 +13,13 @@ module SPI_interface(
                 read_data = 3,
                 read_addr = 4;
 
-    wire started_receiving;
     reg [2:0] state,next;
     reg [3:0] nbit;
-    reg copied;
-    reg [7:0] tx_data_shifted;
 
-    always@(posedge clk) begin
-        if(started_receiving) begin
-            if( ~rst_n || (nbit == 4'd10) ) begin
-                nbit <= 1 ;
-                rx_data <= {9'd0,MOSI} ;
-            end
-            else if((~SS_n) && (nbit < 4'd10)) begin
-                rx_data <= { rx_data[8:0] , MOSI };
-                nbit <= nbit + 1;
-            end
-        end
-        else
-            nbit = 4'd0;
-    end
-
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            tx_data_shifted <= 8'd0;
-            copied <= 0;
-        end 
-        else begin
-            if(tx_valid && ~copied) begin
-                tx_data_shifted <= tx_data;
-                copied = 1;    
-            end
-            else if (~SS_n) begin
-                if(copied)
-                tx_data_shifted <= {tx_data_shifted[6:0], 1'b0};
-            end 
-        end
-    end
+    assign rx_valid = (nbit == 4'd10);
 
     always@(posedge clk or negedge rst_n) begin
-        if(SS_n || ~rst_n)
+        if(~rst_n)
             state <= idle;
         else
             state <= next;
@@ -64,29 +31,66 @@ module SPI_interface(
             CHK  : begin
                 if(SS_n) next = idle;
                 else begin
-                    if(~rx_data[0])
+                    if(~MOSI)
                         next = write;
                     else
                         next = read_addr;
                 end
             end
             write : begin
-                if(SS_n) next = idle;
+                next = (SS_n) ? idle : state;
             end
             read_data : begin
-                if(SS_n) next = idle;
+                next = (SS_n) ? idle : state;
             end            
             read_addr : begin
                 if(SS_n) next = idle;
                 else if(nbit == 4'd10) next = read_data;
+                else next = state;
             end
         default : next = idle;
         endcase
-
     end
 
-    assign MISO = (~SS_n) ? tx_data_shifted[7] : 1'bz;
-    assign rx_valid = (nbit == 4'd10);
-    assign started_receiving = (state != idle) ;
+        
+    always@(posedge clk) begin
+        case(state)
+            idle : begin
+                nbit <= 4'd0;
+                MISO <= 0;
+            end
+            CHK  : begin
+                MISO <= 0;
+                rx_data <= {rx_data,MOSI};
+                nbit <= nbit + 1;
+            end
+            write : begin
+                rx_data <= {rx_data,MOSI};
+                nbit <= nbit + 1;
+                if(nbit==10) begin
+                    nbit <= 0;  
+                end 
+            end
+            read_data : begin
+                MISO <= tx_data [7 - nbit] ;
+                nbit <= nbit + 1 ;
+                if (nbit == 4'h7) begin
+                    nbit <= 0 ;
+                end
+            end            
+            read_addr : begin
+                rx_data <= {rx_data,MOSI};
+                nbit <= nbit + 1;
+                if(nbit==10) begin
+                    nbit <= 0;  
+                end 
+            end
+        default : begin
+            nbit <= 0;
+            rx_data <= 0;
+            MISO <= 0;
+        end
+        endcase
 
+    end
 endmodule
